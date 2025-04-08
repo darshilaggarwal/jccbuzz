689dd3fc491710c01e7873c4383a9e512b8317e4
darshilaggarwal <darshilaggarwal55@gmail.com>
2025-04-08 14:06:08 +0530

    edit message in chat applied

--- models/chat.js
+++ models/chat.js
@@ -50,6 +50,10 @@ const messageSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    isEdited: {
+        type: Boolean,
+        default: false
+    },
     createdAt: {
         type: Date,
         default: Date.now
